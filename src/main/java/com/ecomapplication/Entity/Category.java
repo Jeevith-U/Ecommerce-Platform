8ad178b356c20d33bc17b58ce57b3cc4c896813c
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-10-27 13:38:39 +0530

    Stared to work with Category and Added the needed dependency

---
+++ src/main/java/com/ecomapplication/Entity/Category.java
@@ -0,0 +1,27 @@
+package com.ecomapplication.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @NotBlank
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
+}
