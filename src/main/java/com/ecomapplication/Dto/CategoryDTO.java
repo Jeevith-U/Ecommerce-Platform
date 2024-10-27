fb8900539388c1022c45db7a84fd9d8cf54ef76b
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-10-27 16:15:12 +0530

    Added the Dto's And Mappers

---
+++ src/main/java/com/ecomapplication/Dto/CategoryDTO.java
@@ -0,0 +1,19 @@
+package com.ecomapplication.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+
+    private Long categoryId;
+    private String categoryName;
+
+}
---
+++ src/main/java/com/ecomapplication/Dto/ProductDTO.java
@@ -0,0 +1,25 @@
+package com.ecomapplication.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDTO {
+
+    private Long productId;
+    private String productName;
+    private String image;
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
+
+}
