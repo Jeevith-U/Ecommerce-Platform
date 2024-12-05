9476547f09cebc3f06f822ad28c1df08013a18f4
Jeevith-U <102320065+Jeevith-U@users.noreply.github.com>
2024-12-05 15:54:25 +0530

    Completed all the CRUD operations related to Adress Entity

---
+++ src/main/java/com/ecomapplication/Controller/AddressController.java
@@ -0,0 +1,66 @@
+package com.ecomapplication.Controller;
+
+import com.ecomapplication.Dto.AddressDTO;
+import com.ecomapplication.Entity.User;
+import com.ecomapplication.Service.AddressService;
+import com.ecomapplication.Util.AuthUtil;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class AddressController {
+
+    @Autowired
+    private AddressService addressService ;
+
+    @Autowired
+    private AuthUtil authUtil ;
+
+    @PostMapping("/addresses")
+    public ResponseEntity<AddressDTO> createAddress(@Valid @RequestBody AddressDTO addressDTO){
+        User user = authUtil.loggedInUser();
+        AddressDTO savedAddressDTO = addressService.createAddress(addressDTO, user);
+        return new ResponseEntity<>(savedAddressDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/addresses")
+    public ResponseEntity<List<AddressDTO>> getAddresses(){
+        List<AddressDTO> addressList = addressService.getAddresses();
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
+    }
+
+    @GetMapping("/addresses/{addressId}")
+    public ResponseEntity<AddressDTO> getAddressById(@PathVariable String addressId){
+        AddressDTO addressDTO = addressService.getAddressesById(addressId);
+        return new ResponseEntity<>(addressDTO, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/users/addresses")
+    public ResponseEntity<List<AddressDTO>> getUserAddresses(){
+        User user = authUtil.loggedInUser();
+        List<AddressDTO> addressList = addressService.getUserAddresses(user);
+        return new ResponseEntity<>(addressList, HttpStatus.OK);
+    }
+
+    @PutMapping("/addresses/{addressId}")
+    public ResponseEntity<AddressDTO> updateAddress(@PathVariable String addressId
+            , @RequestBody AddressDTO addressDTO){
+        AddressDTO updatedAddress = addressService.updateAddress(addressId, addressDTO);
+        return new ResponseEntity<>(updatedAddress, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/addresses/{addressId}")
+    public ResponseEntity<String> updateAddress(@PathVariable String addressId){
+        String status = addressService.deleteAddress(addressId);
+        return new ResponseEntity<>(status, HttpStatus.OK);
+    }
+
+
+}
---
+++ src/main/java/com/ecomapplication/Repository/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.ecomapplication.Repository;
+
+import com.ecomapplication.Entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, String> {
+}
